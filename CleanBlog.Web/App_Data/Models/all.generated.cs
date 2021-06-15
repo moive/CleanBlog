70d385ca341794a88d8a2584f9c0bc6b80eaedb0
Moises Velasquez <mvelasquez@pictoric.com>
2021-06-14 23:55:49 -0500

    added other template in project

--- CleanBlog.Web/App_Data/Models/all.generated.cs
+++ CleanBlog.Web/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "708535f51137280c")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 
 // FILE: models.generated.cs
