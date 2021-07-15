9580e3f9ea9bc38f7a2423d72b9e5be3634b8f76
Moises Velasquez <mvelasquez@pictoric.com>
2021-07-14 23:28:27 -0500

    latest articles with paging

---
+++ CleanBlog.Core/Helpers/QueryStringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+
+namespace CleanBlog.Core.Helpers
+{
+    public static class QueryStringHelper
+    {
+        public static int GetIntFromQueryString(HttpRequestBase request, string key, int fallbackValue = 0) {
+            var stringValue = request.QueryString[key];
+            if (stringValue != null && !string.IsNullOrWhiteSpace(stringValue) && int.TryParse(stringValue, out var numericValue))
+            {
+                return numericValue;
+            }
+            return fallbackValue;
+        }
+    }
+}
--- CleanBlog.Web/App_Data/Models/models.generated.cs
+++ CleanBlog.Web/App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder.Embedded;
 
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "f469613efe8b6d56")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedModels
 {
